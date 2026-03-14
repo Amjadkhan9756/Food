cfeaed96cdcd8674f54ce6c5c1e7eeb3dfce9925
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-03-14 12:04:19 +0530

    new update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,13 @@ const express =require("express");
 const cookieParer=require("cookie-parser");
 const foodRoutes = require("./routes/food.routes.js");
 const authRoutes=require("./routes/auth.routes.js")
+const cors = require("cors");
 const app=express();
 
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(cookieParer());
 app.use(express.json());
 
