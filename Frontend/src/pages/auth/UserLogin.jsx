4000d562f39d5eb9b78dab73b2b3ba2b1a27bd99
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-03-28 11:03:07 +0530

    reistration css completed

---
+++ Frontend/src/pages/auth/UserLogin.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import "../../Style/auth-shared.css";
+
+
+function UserLogin(){
+    return(
+        <>
+        <h1>hello </h1>
+        </>
+    )
+}
+
+export default UserLogin;
--- Frontend/src/routes/AppRouter.jsx
+++ Frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import UserRegister from '../pages/auth/UserRegister'
 import Home from '../pages/general/Home';
+import UserLogin from '../pages/auth/UserLogin';
 
 
 
@@ -13,6 +14,7 @@ function AppRoutes(){
                 
                 <Route path='/user/register' element={<UserRegister/>}/>
                                 <Route path='/' element={<Home/>}/>
+                <Route path='/user/login' element={<UserLogin/>}/>
 
             </Routes>
         </Router>
