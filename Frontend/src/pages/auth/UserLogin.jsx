07c9c730d685a84fc8e6f7c651929547b8bbd653
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-03-29 10:10:18 +0530

    login

--- Frontend/src/pages/auth/UserLogin.jsx
+++ Frontend/src/pages/auth/UserLogin.jsx
@@ -6,9 +6,45 @@ import "../../Style/auth-shared.css";
 
 
 function UserLogin(){
+    const navigate=useNavigate();
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const response = await axios.post("http://localhost:8080/api/auth/user/login",{
+            email,
+            password
+        },{withCredentials:true});
+
+        console.log(response.data);
+        navigate("/");//redirect to home after login
+    }
     return(
         <>
-        <h1>hello </h1>
+        <div className='auth-page-wrapper'>
+            <div className='auth-card ' role='region' aria-lebelledby="user-login-title">
+                <header>
+                    <h1 id='user-login-title' className='auth-title'>Welcome back</h1>
+                    <p className='auth-subtitle'>Sign in to continue your food journey.</p>
+                </header>
+                <from className="auth-from" onSubmit={handleSubmit} noValid>
+                    <div className='field-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input id='email' name="email" type='email' placeholder='you@example.com' autoCapitalize='email'></input>
+                    </div>
+                    <div className='field-group'>
+                        <label htmlFor='password'>Password</label>
+                        <input id='password' name='password' type='password' placeholder='password' autoCapitalize='current-password'></input>
+                    </div>
+                    <button className='auth-submit' type='submit'>Sign In</button>
+                </from>
+                <div className='auth-alt-action'>
+                    New here? <a href='/user/register'>Create accont</a>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
