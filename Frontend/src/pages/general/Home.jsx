a24b19055363996a9b8232f621de8f6c031ff59d
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-04-02 09:48:54 +0530

    creating like component for liking videos

--- Frontend/src/pages/general/Home.jsx
+++ Frontend/src/pages/general/Home.jsx
@@ -16,6 +16,18 @@ const Home = ()=>{
 
          })
     },[])
+
+    async function likeVideo(item){
+        const response = await axios.post("http://localhost:8080/api/food/like",{foodId:item._id},{withCredentials:true})
+         if(response.data.like){
+            console.log("Video liked");
+            setVideos((prev)=>prev.map((v)=>v._id===item._=== item._id? {...v,likeCount:v.likeCount+1}:v))
+         }
+         else{
+            console.log("Video unliked");
+            setVideos((prev) => prev.map((v) => v._id === item._id ? { ...v, likeCount: v.likeCount - 1 } : v))
+         }
+    }
     return (
         <>
        <h1>heloo</h1>
