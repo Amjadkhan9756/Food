d543575bbea8f6a6178721a4326c90009fbf5afa
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-04-10 10:46:47 +0530

    uploading videos

--- Frontend/src/routes/AppRouter.jsx
+++ Frontend/src/routes/AppRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import UserRegister from '../pages/auth/UserRegister'
 import Home from '../pages/general/Home';
 import UserLogin from '../pages/auth/UserLogin';
-import BottomNav from '../../../../Youtube-project-food-view/frontend/src/components/BottomNav';
+import Saved from '../pages/general/Saved';
 
 
 
@@ -14,8 +14,9 @@ function AppRoutes() {
                 <Routes>
 
                     <Route path='/user/register' element={<UserRegister />} />
-                    <Route path='/' element={<><Home /> <BottomNav/></>} />
+                    <Route path='/' element={<Home />} />
                     <Route path='/user/login' element={<UserLogin />} />
+                    <Route path="/saved" element={<Saved />} />
 
                 </Routes>
             </Router>
